118d701f67da81a3b97eab8645c4ac4986589be4
Rodrigo Gutierrez Morquecho <Morquecho>
2026-06-05 16:36:49 -0700

    Version 4: Finished CV basic :D

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("BIENVENIDO")
+const btnTema = document.getElementById("btnTema");
+
+btnTema.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    btnTema.textContent = "Modo Claro";
+  } else {
+    btnTema.textContent = "Modo Oscuro";
+  }
+});
